188ca855098028bb199edfc45480bea3fd1c9e48
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 18:42:51 +0000

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ( n >= 0 ) {
+	if (n >= 0)
 		printf(" is positive\n");
-	}
-	else if ( n == 0 ) {
-		printf(" is zero\n"); 
-	}
-	else {
+	else if (n == 0)
+		printf(" is zero\n");
+	else
 		printf(" is negative\n");
-	}
 	return (0);
 }
