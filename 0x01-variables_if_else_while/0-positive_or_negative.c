243bd7c638e25cbe17e40bf04d9f26e759bc6e56
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 17:58:28 +0000

    if else while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main = entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	if n = rand() - RAND_MAX / 2;
+
+	return (0);
+}
