7429b468a806e868084edef287c46e1d9cc5b61d
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 02:55:29 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc puts(const char Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+int main ()
+{
+	char string [] = "Programming is like building a multili	ngual puzzle";
+	puts (string);
+}
