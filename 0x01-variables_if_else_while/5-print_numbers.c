59ed5bfcc8ebfbcce2915f883afe334f399b3505
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 23:49:24 +0000

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 0; count <= 9; count++)
+		printf("%d", count);
+	printf("\n");
+
+	return (0);
+}
