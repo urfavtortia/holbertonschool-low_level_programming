319dd3a76a24e90b9187bff4655efdc7e67da5ae
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 03:42:39 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
 *  main - entry
 */
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-};
+        return (0);
+}
