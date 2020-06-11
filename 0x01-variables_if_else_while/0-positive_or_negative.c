b93ea6e079d57a900688f443f1e583c84c64bf14
urfavtortia <{tiainreallife@gmail.com}>
2020-06-11 18:16:56 +0000

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,12 +9,9 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	cout << "Enter the number to be checked : ";
-	cin >> n;
 	if (n >= 0)
-		cout << n << " is a positive number.";
+		cout << n << " is positive";
 	else
-		cout << n << " is a negative number.";
+		cout << n << " is negative";
 	return (0);
 }
