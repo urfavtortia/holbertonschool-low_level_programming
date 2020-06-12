e3abb94778b33fb3ae1ad9a60b6ee287f1ac22ce
urfavtortia <{tiainreallife@gmail.com}>
2020-06-12 02:20:55 +0000

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= 9; x++)
+		putchar(x);
+	for (y = 'f'1; y <= 'f'; x++)
+		putchar(y);
+	putchar('\n');
+
+	return (0);
+}
