084ec7ca8312c4492e5c64cc0b8fdb1a99466454
urfavtortia <{tiainreallife@gmail.com}>
2020-06-17 03:52:15 +0000

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		int x;
+
+		for (x = 97; x <= 122; x++)
+		{
+			putchar(x);
+		}
+
+	putchar('\n');
+	}
+
+return (0);
+}
