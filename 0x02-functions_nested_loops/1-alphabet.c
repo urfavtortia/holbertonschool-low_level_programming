c952c88b6cc03b72cd8fb955e5226eabaf927b43
urfavtortia <{tiainreallife@gmail.com}>
2020-06-19 04:02:37 +0000

    a-z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
 #include "holberton.h"
 /**
- * main - Entry Point
- *
- * Return: 0
- */	
-int main(void)
+ * print_alphabet - prints alphabet a - z in lowercase letters
+ */
+void print_alphabet(void)
+
+
 {
+	int alpha;
 
-	void print_alphabet(void);
-	{
-		int x;
-
-		for (x = 97; x <= 122; x++)
-		{
-			_putchar(x);
-		}
+	for (alpha = 97; alpha <= 122; alpha++)
+		_putchar(alpha);
 
 	_putchar('\n');
-	}
-
-return (0);
 }
