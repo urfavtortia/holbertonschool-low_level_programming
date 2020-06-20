714ae302091f125b379c82b0620d767172fc9b93
urfavtortia <{tiainreallife@gmail.com}>
2020-06-20 22:24:02 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - multiplies int a by int b
+ * @a: vairable to be tested
+ * @b: variable to be tested
+ * Return: value of c
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
