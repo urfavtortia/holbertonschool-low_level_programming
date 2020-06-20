dc413db37edd6e30b1c03768dc3a9d08d84b30c6
urfavtortia <{tiainreallife@gmail.com}>
2020-06-20 22:36:21 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_numbers - prints numbers 0 through 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+		_putchar(x);
+
+		_putchar('\n');
+
+}
