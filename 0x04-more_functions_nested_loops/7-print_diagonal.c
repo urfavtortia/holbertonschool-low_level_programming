5cfb23e076d69473a3c9add79a4ca95aaefce8c6
urfavtortia <{tiainreallife@gmail.com}>
2020-06-21 14:51:56 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print a diagonal line
+ * @n : variable used
+ *
+ * Return: null
+ */
+void print_diagonal(int n)
+{
+	int p, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 0; p < n; p++)
+		{
+			for (s = 0; s < p; s++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+			s = 0;
+		}
+	}
+	return;
+}
