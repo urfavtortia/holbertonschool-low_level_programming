276d90bde23725818237a73b8135819c1dedab6f
urfavtortia <{tiainreallife@gmail.com}>
2020-06-23 06:40:01 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle with n length sides
+ *
+ * @size : size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j < (size - i); j++)
+			{
+					_putchar(' ');
+			}
+			for (k = j; k <= size; k++)
+			{
+				_putchar('#');
+			}
+
+		_putchar('\n');
+		}
+	}
+}
