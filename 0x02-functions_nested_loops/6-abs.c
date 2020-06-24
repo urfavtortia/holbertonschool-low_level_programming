a2a53778e7ca73e5cb88c4b82de0bb66bc08fc8d
urfavtortia <https://github.com/urfavtortia>
2020-06-24 22:39:05 +0000

    absolute val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value of int
+ * @int - value
+ * Description - will print absolute value
+ * Return: 0
+ */
+int _abs(int i)
+{
+	if (i <= 0)
+	{
+		i = (i * -1);
+
+		return (i);
+	}
+
+	else if (i >= 0)
+	{
+		i = (i * 1);
+
+		return (i);
+	}
+return (0);
+}
