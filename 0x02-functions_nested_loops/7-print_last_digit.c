09ec76e2b309075cfb6ac03eaf5f34f851602ca7
urfavtortia <https://github.com/urfavtortia>
2020-06-24 23:02:58 +0000

    pld

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of int
+ * @i: value
+ * Return: 0
+ */
+int print_last_digit(int i)
+{
+	if (i < 0)
+	{
+		i = (i * -1);
+		_putchar((i % 10) + '0');
+	}
+
+	else
+	{
+		_putchar((i % 10) + '0');
+	}
+
+return (0);
+}
