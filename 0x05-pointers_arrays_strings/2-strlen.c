92f8ef5c3f933d2bc8c8715498d6937310776ea7
urfavtortia <{tiainreallife@gmail.com}>
2020-06-25 22:05:14 +0000

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+/**
+ * _strlen - prints the string length
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i, len;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+
+	return (len);
+}
