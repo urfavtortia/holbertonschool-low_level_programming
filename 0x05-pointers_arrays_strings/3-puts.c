addeb39d602a8f5e04ce02938f76b05f02dbdac9
urfavtortia <{tiainreallife@gmail.com}>
2020-06-26 03:54:22 +0000

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+/**
+ * _puts - prints string
+ * @str: string to print
+ * Return: str
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != ('\0'); i++)
+		str++;
+}
