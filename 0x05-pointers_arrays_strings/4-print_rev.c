853eff6cca8007071c57d86d577414c4003c3ccd
urfavtortia <{tiainreallife@gmail.com}>
2020-07-04 08:46:13 +0000

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+/**
+ * print_rev - prints str in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	while (s[len] != 0)
+	{
+		len++;
+ 	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
