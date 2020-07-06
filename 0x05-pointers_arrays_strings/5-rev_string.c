7090042115c00fe80293afbcef4f0b64aa091538
urfavtortia <{tiainreallife@gmail.com}>
2020-07-06 02:45:17 +0000

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+/**
+ * rev_string: reverses string
+ *
+ *
+ *
+ */
+void rev_string(char *s)
+{
+	int temp;
+	int *end;
+
+	for (end = 0; *end != '\0'; end++);
+	for (s = 0; s < end / 2; s++)
+	{
+		temp = *s;
+		*s = *end;
+		*end = temp;
+	}
+	for (s = 0; s < end; s++);
+	_putchar('\n');
+}
